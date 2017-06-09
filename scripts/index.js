bdd53df300dee4a7f0960813badfbb1d188f6040
PibeDx <jcuentast@gmail.com>
2017-06-09 01:24:12 -0500

    feat: component  - call service  - send data to component

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,27 @@
 Vue.component('movie', {
-	template: '#single-movie'
+	template: '#single-movie',
+	props: ['selectedMovie']
 });
 
 var vm = new Vue({
 	el: '#root',
 	data: {
-		title: 'Hello World!'
+		title: 'Hello World!',
+		selectedMovie: {}
+	},
+	mounted: function() {
+		this.searchMovie('indiana');
+	},
+	methods: {
+		searchMovie: function(movie) {
+			fetch('http://www.omdbapi.com/?t=' + movie)
+			.then(function(response) {
+				return response.json();
+			}).then(json => {
+				console.log(json);
+				this.selectedMovie = json;
+			});
+
+		}
 	}
 });
